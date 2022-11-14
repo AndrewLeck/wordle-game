42f3f3aa12d88112d6538679c08ff11607e65d78
Andrew Leck <Aleck360@gmail.com>
2022-11-14 15:11:51 -0500

    created css, and js files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+    function createSquares() {
+        const gameboard = document.getElementById("board")
+
+        for ( let i = 0; i< 30; i++){
+            let square = document.createElement
+        }
+
+    }
+})
