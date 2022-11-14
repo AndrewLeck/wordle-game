34606c381f37352d73d83764157bde110547b495
Andrew Leck <Aleck360@gmail.com>
2022-11-14 15:15:50 -0500

    created a function that makes 30 squares appear onthe DOM

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", () =>{
 
-    function createSquares() {
-        const gameboard = document.getElementById("board")
+    createSquares();
 
+    function createSquares() {
+        const gameBoard = document.getElementById("board")
+        
+        //This forloop will created 30 squares on my index
         for ( let i = 0; i< 30; i++){
-            let square = document.createElement
+            let square = document.createElement("div")
+            square.classList.add("square");
+            square.setAttribute("id", i + 1);
+            gameBoard.appendChild(square);
         }
 
     }
-})
+});
